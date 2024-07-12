8d6a16ce119f55506012aff0048a6416d1ec769e
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-07-12 18:23:14 -0400

    Correct base64 chars

--- IPA_Keyboard_Router_Server.java
+++ IPA_Keyboard_Router_Server.java
@@ -152,7 +152,7 @@ public int pythonClientEvent(sClient C) {
   }
 
   String rand64Str(int length) {
-    final char[] ALPHABET = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', '!', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', '/', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'i', 'i', 'i', 'i', 'i', 'i', 'i', 'i', 'i', 'i', '-', '_'};
+    final char[] ALPHABET = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_'};
     String out = "";
     for (int i = 0; i < length; i++) {
       out += ALPHABET[rand.nextInt(64)];
